b5a01572777c58c38a7f4c8730035aab63f2cf61
yani82 <yani.iman1@gmail.com>
2021-05-31 03:10:30 -0400

    Create db schema

--- db/migrate/20210530073844_create_travelbugs.rb
+++ db/migrate/20210530073844_create_travelbugs.rb
@@ -2,6 +2,8 @@ class CreateTravelbugs < ActiveRecord::Migration[6.0]
   def change
     create_table :travelbugs do |t|
       t.string :body 
+      t.integer :user_id 
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_073844) do
+ActiveRecord::Schema.define(version: 2021_05_31_070044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "travelbugs", force: :cascade do |t|
     t.string "body"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
